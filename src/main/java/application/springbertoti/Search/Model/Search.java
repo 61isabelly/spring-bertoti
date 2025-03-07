0266b3bae7ad904a66945af48429dcdceb00a91b
Isabelly S <164695350+61isabelly@users.noreply.github.com>
2025-03-07 14:00:13 -0300

    refactor: adjusting project to associate database

--- src/main/java/application/springbertoti/Search/Model/Search.java
+++ src/main/java/application/springbertoti/Search/Model/Search.java
@@ -1,23 +1,46 @@
 package application.springbertoti.Search.Model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Data
 @Table(name = "search")
 public class Search {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "id")
     private Integer id;
-
-    @Column (name = "name")
     private String name;
-
-    @Column (name = "description")
     private String description;
-
-    @Column (name = "category")
     private String category;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
